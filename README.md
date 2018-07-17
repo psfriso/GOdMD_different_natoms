7fa5b882407ebaddcac27b4ac8d532f487973634
psfriso <psfriso@cartrawler.com>
2018-07-17 09:20:24 +0100

    adding description for parameters

--- README.md
+++ README.md
@@ -4,3 +4,28 @@ cd dat/2lao_A-1lst_A-1/
 
 ../../discrete/discrete -i dmdtest.in -pdbin 2lao_A.REF.pdb -pdbtarg 1lst_A.pdb -p1 2lao_A.aln -p2 1lst_A.aln -trj 2lao_A-1lst_A_trajectory.crd -ener energy.dat -o 2lao_A-1lst_A_1.log
 
+
+### Parameters description from ```discrete/paramSet.f```
+
+- TSNAP: Frequency to output structures in the simulation. Low number gives more structures.
+
+- TRECT: Frequency to apply the Maxwell-Demon algorithm
+
+- TACT: Frequency to update the bonded distances.
+
+- ACCEPTANCE: Fraction of sampled structures that would be accepted in Monte Carlo test. Low values give biased results. High values may lead to convergence problems. I recommend values between 0.5 and 0.8
+
+- GOENER: Energy depth of the well kcal/mol
+
+- SIGMAGO: Width of bonded bonds, in Armstrongs.
+
+- WELLWIDTH: Width of non-bonded bonds, in Armstrongs.
+
+- RGYRPERCENT: Maximum distance to consider non-bonded and changing interactions (double go wells) as a fraction of the average radius of gyration
+
+- RCUT1: Maximum distance to consider non-bonded and non-changing interactions (single go wells)
+
+- MINWENERWELL: Minimum energy interaction per well. Below this number the pseudo-Metadynamics algorithm will ignore this interaction.
+
+- SIGMAFAKE: Bonded distance for non-consecutive particles (a gap in the structure)
+ 
--- discrete/paramSet.f
+++ discrete/paramSet.f
@@ -24,46 +24,48 @@
       tact = 5., &
       trect = 100., &
       tini = 0.
-      
-  real,save :: acceptance=0.77 ! DEFAULT 0.65 NO MODIFICAR MUCHO. (0.5-0.8) 
-  real :: xbeta=5.0000 !2
-  real :: goener=0.25 !*8.314!     0.50
-  real :: sigmago=0.1000
-  real *8 :: dtol=0.30000
-  real :: wellWidth=0.1300            ! DEFAULT 0.13
-  real :: scalFactor=0.300   !40
-  real :: rgyrPercent=0.500           ! DEFAULT=0.52
-  real *8 :: rcut2GO4=0.0             !  depends on radius gyration
-  real *8 :: rcut1= 70.00 !70.00 was the original value, 100 works better. switching back to 70 for consitiency with previous results
-  real *8 :: mxRcut4go=12.0
-  real *8 :: ssectol=dble(0.035)
-  real :: minEnerWell=0.10   !*8.314         ! DEFAULT 0.05
- LOGICAL :: StartMD=.FALSE.        ! IGUAL QUE ENER_evo_size
- INTEGER :: error_evo_size=100      ! 20 DEFAULT number of rmsd point to use in linear regression
- INTEGER :: Ener_evo_size=120         ! DEFAULT 15
- INTEGER , PARAMETER :: NEVECS=5
- real*8 :: hstep=1.0
- REAL , PARAMETER :: DEGO_BASE=0.03 ! 5 porciento del pozo a sacar en cada metaDMD
- REAL :: errorAcceptable=1.00 ! 1.25 should be the value
- INTEGER :: nskip=5
- REAL :: sigmaFake=0.02
- CHARACTER(LEN=20) :: fileref='reference.pdb'
- INTEGER:: natomMin=50
- real:: RMSD_DIF=0.01 ! A
- real*8::rcontacts=50.0d0
 
+! Parameters - dont modify them
+real :: xbeta=5.0000 !2
+INTEGER , PARAMETER :: NEVECS=5
+real *8, parameter :: dtol=0.30000
+real *8 :: rcut2GO4=0.0
+real *8 :: ssectol=dble(0.035)
+REAL , PARAMETER :: DEGO_BASE=0.03 ! 5 porciento del pozo a sacar en cada metaDMD
+INTEGER:: natomMin=50
+LOGICAL :: StartMD=.FALSE.
+real :: scalFactor=0.300
+
+
+! Parameters - feel free to explore them
+real,save :: acceptance=0.77 ! DEFAULT 0.65 NO MODIFICAR MUCHO. (0.5-0.8)
+real :: goener=0.25
+real :: sigmago=0.1000
+real :: wellWidth=0.1300            ! DEFAULT 0.13
+real :: rgyrPercent=0.500           ! DEFAULT=0.52
+real *8 :: rcut1= 70.00 !70.00 was the original value, 100 works better. switching back to 70 for consitiency with previous results
+real *8 :: mxRcut4go=12.0
+real :: minEnerWell=0.10   !*8.314         ! DEFAULT 0.05
+INTEGER :: error_evo_size=100      ! 20 DEFAULT number of rmsd point to use in linear regression
+INTEGER :: Ener_evo_size=120         ! DEFAULT 15
+real*8 :: hstep=1.0
+REAL :: errorAcceptable=1.00 ! 1.25 should be the value
+INTEGER :: nskip=5
+REAL :: sigmaFake=0.02
+real:: RMSD_DIF=0.01 ! A
+real*8::rcontacts=50.0d0
 LOGICAL :: writingStandard = .TRUE.
 
 CONTAINS
 !===============================================================================
  subroutine readInputParamSet (unit_i)
    integer, intent(IN) :: unit_i
-!   
+!
    namelist /input/ tsnap,temp,seed,nbloc,scalFactor,Ener_evo_size,&
    Ener_evo_size,nskip,tact,errorAcceptable,writingStandard,fileref,natomMin
 !
    read (unit_i, INPUT)
-   ! checking 
+   ! checking
    if (TRECT.gt.TSNAP) TRECT = TSNAP
    if (TCORR.gt.TRECT) TCORR = TRECT
    if (TACT.gt.TCORR)  TACT = TCORR
